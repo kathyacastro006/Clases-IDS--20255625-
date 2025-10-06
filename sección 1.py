901fc36517540d22949674d9eb778c5c192c4635
kathyacastro006 <kcastro52006@gmail.com>
2025-10-06 13:34:06 -0600

    Create sección 1.py

---
+++ sección 1.py
@@ -0,0 +1,17 @@
+nombre = "Kathya"
+print(nombre)
+edad = "19"
+ciudad = "Santa Tecla"
+profesion = "estudiante"
+print(f"mi nombre es {nombre}, tengo {edad} años. Vivo en {ciudad} y soy {profesion}")
+#una variable sirve para almacenar información temporal
+mensaje = "Bienvenido al curso de Python"
+print(mensaje)
+anio_actual = 2025
+anio_nacimiento = 2006
+print(anio_actual - anio_nacimiento)
+pi = 3.14159265359
+print(f"el valor de pi es {pi: .5}")
+#el tipo de variable entero es int, por lo que fuerza a que se convierta en un #entero, pero una variable de cadena es un str, o sea solo de texto.
+
+
