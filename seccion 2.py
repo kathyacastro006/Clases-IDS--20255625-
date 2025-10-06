30ea65457b066a810796db45d5e607cc1d16aa35
kathyacastro006 <kcastro52006@gmail.com>
2025-10-06 16:06:19 -0600

    Create seccion 2.py

---
+++ seccion 2.py
@@ -0,0 +1,28 @@
+"""nombre = input("Hola! ingrese su nombre: ")
+edad = int(input("ingrese su edad: "))
+print(edad*2)
+numero_entero1= int(input("ingrese un número entero: "))
+numero_entero2 = int(input("ingrese otro número entero: "))
+suma = numero_entero1 + numero_entero2 
+print(suma)
+numero_decimal = float((input("ingrese un número decimal: ")))
+division_ = numero_decimal / 2
+print(division_)
+ano_actual = 2025
+edad = int(input("ingrese su año de nacimiento: "))
+edad_actual = ano_actual - edad
+print(edad_actual)
+precio = eval((input("ingrese el precio del producto: $"))) #eval es que el valor que tome lo deje como esté
+unidades = int(input("ingrese la cantidad de unidades: "))
+precio_total = precio * unidades
+print(precio_total)
+numero_entero3 = int(input("ingrese un número entero: "))
+cuadrado = numero_entero3 * numero_entero3
+print(cuadrado)"""
+"""numero_promedio1 = float((input("ingrese un número: ")))
+numero_promedio2 = float((input("ingrese otro número: ")))
+promedio_total = ((numero_promedio1 + numero_promedio2)/2)
+print(promedio_total)"""
+nombre_completo = input("escriba su nombre completo: ")
+edad_persona = input("escriba su edad: ")
+print(f"Hola, {nombre_completo}. Tienes {edad_persona} años.")
