44c2a1793baaa2bd6592cb983fd477b5c6a6128c
kathyacastro006 <kcastro52006@gmail.com>
2025-10-07 14:37:00 -0600

    Update seccion 3.py

--- seccion 3.py
+++ seccion 3.py
@@ -10,10 +10,19 @@ mayor_igualque = numero > 5
 print(mayor_igualque)
 menor_igualque = numero < 5
 print(menor_igualque)"""""
-x = 10
+"""x = 10
 y = 10
 print(x is y)
-print(x is not y)
-numero_par_impar = int(input("escriba un número, por favor: "))
- 
- 
+"""numero_par_impar = int(input("escriba un número, por favor: "))
+print(es_par)
+#crea una variable booleana registrado = True. Usa operadores lógicos (and, or, not) para mostrar combinaciones posibles."""
+"""numero1 = input("escriba un número: ")
+numero2 = input("escriba otro número: ")
+print((int(numero1) > int(numero2)))"""
+
+"""edad_persona = input("por favor, ingrese su edad: ")
+print(int(edad_persona) > int(18))"""
+
+edad = 25
+estudiante = True
+print(edad >= 18 and estudiante)
