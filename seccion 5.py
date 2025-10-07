b4d3b49a04eb09960c3b09df135a26b6a09b00e8
kathyacastro006 <kcastro52006@gmail.com>
2025-10-07 16:18:06 -0600

    Create seccion 5.py

---
+++ seccion 5.py
@@ -0,0 +1,20 @@
+"""palabra = "python"
+print(palabra [0])
+print(palabra [0:3])
+print(palabra [4:6])"""""
+
+"""frase = "Aprender python es divertido"
+print(frase [9:15:1]) #preguntar
+
+palabra = input("escriba una palabra: ")
+print(len(str(palabra)))"""
+
+"""word = input("escriba una palabra: ") #preguntar a Alvin como hace el paso 4"""
+
+
+codigo = "ABC1234XYZ"
+print(codigo[3:7:1])
+
+
+nombre = "Juan Perez"
+print(nombre[5:10:1])
