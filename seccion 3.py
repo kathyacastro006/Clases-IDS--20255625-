fd018e6d4478372088ff436310d583546b624de9
kathyacastro006 <kcastro52006@gmail.com>
2025-10-07 13:56:32 -0600

    Create seccion 3.py

---
+++ seccion 3.py
@@ -0,0 +1,19 @@
+"""a = 10
+b = 3
+c = 5
+print(a+b)
+print(a/b)
+print(a%c)
+numero = 7
+
+mayor_igualque = numero > 5
+print(mayor_igualque)
+menor_igualque = numero < 5
+print(menor_igualque)"""""
+x = 10
+y = 10
+print(x is y)
+print(x is not y)
+numero_par_impar = int(input("escriba un número, por favor: "))
+ 
+ 
