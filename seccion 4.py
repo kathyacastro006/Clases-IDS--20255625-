015c70657b0e72f3679870fed8cab0b80541a49c
kathyacastro006 <kcastro52006@gmail.com>
2025-10-07 15:32:31 -0600

    Create seccion 4.py

---
+++ seccion 4.py
@@ -0,0 +1,25 @@
+"""nombre = "kathya"
+producto = "celular"
+precio = 600
+print(f"hola, {nombre}, el precio del {producto} es de ${precio}")"""""
+
+"""nombre_persona = input("ingrese su nombre: ")
+pais = input("ingrese su país: ")
+print(f"hola, {nombre_persona}, de {pais}. Bienvenid@ :)")"""
+
+"""nombre1 = input("ponga su nombre: ")
+edad = input("escriba su edad: ")
+ciudad = input("escriba donde vive: ")
+print(f"resumen de información: 1. nombre: {nombre1}")
+print(f"edad: {edad}")
+print(f"ciudad: {ciudad}")"""
+
+"""altura = input("escriba la altura: ")
+base = input("escriba la base: ")
+print(f"area: {(int(altura)*int(base))/2:.2f}")"""""
+
+producto = "pan"
+unidades = "5"
+precio = "4.5"
+precio_producto = "22.5"
+print(f"producto:{producto},precio: ${precio}, cantidad: {unidades}, total:${precio_producto} ")
