cb46b5198e289f9fea35d85c05edba52611a95d1
kathyacastro006 <kcastro52006@gmail.com>
2025-10-07 16:30:09 -0600

    Create seccion 6.py

---
+++ seccion 6.py
@@ -0,0 +1,12 @@
+"""frase = "La programación es poderosa"
+print("poderosa" in frase)
+print("Java" not in frase)"""
+
+"""frase2 = "hola"
+print(frase2 * 8)"""
+
+"""palabra = "banana"
+print(palabra.count("a"))"""
+
+mensaje = "'El Salvador es un gran país"
+print(mensaje.index("gran"))
